cdc61e99985614ebeb4a99854fa4f5ca4c0d3a31
Khiem <aidolamgidi@gmail.com>
2025-06-26 12:25:30 +0700

    Crud suggestionRule

--- Server.Domain/Entities/SuggestionRule.cs
+++ Server.Domain/Entities/SuggestionRule.cs
@@ -8,11 +8,17 @@ namespace Server.Domain.Entities
 {
     public class SuggestionRule : BaseEntity
     {
-        public string WeekSuggest {  get; set; }
-        //public Guid TargetNutrientId { get; set; } Chưa cần // (nullable) Nếu là để bổ sung vitamin đặc biệt
+        public int? MinWeek { get; set; }
+        public int? MaxWeek { get; set; }
+        public int? Trimester { get; set; }
 
-        public string Condition { get; set; } //Mô tả điều kiện: ví dụ "thiếu máu", "thiếu sắt"
-        public string Note { get; set; } //Ghi chú cho bác sĩ/chuyên gia
-        public IEnumerable<Food> Foods { get; set; }
+        public string Condition { get; set; }
+        public string Note { get; set; }
+
+        public ICollection<Food> Foods { get; set; }
+
+        public ICollection<Disease> Diseases { get; set; }
+
+        public bool IsPositive { get; set; }
     }
 }
--- Server.Infrastructure/Repositories/SuggestionRuleRepository.cs
+++ Server.Infrastructure/Repositories/SuggestionRuleRepository.cs
@@ -3,38 +3,37 @@ using Server.Application.Interfaces;
 using Server.Application.Repositories;
 using Server.Domain.Entities;
 using Server.Infrastructure.Data;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Server.Infrastructure.Repositories
 {
     public class SuggestionRuleRepository : GenericRepository<SuggestionRule>, ISuggestionRuleRepository
     {
-        public SuggestionRuleRepository(AppDbContext dbContext,
-            ICurrentTime timeService,
-            IClaimsService claimsService)
-            : base(dbContext,
-                  timeService,
-                  claimsService)
+        private readonly AppDbContext _context;
+
+        public SuggestionRuleRepository(AppDbContext context, ICurrentTime currentTime, IClaimsService claimsService)
+            : base(context, currentTime, claimsService)
         {
+            _context = context;
         }
 
-        public async Task<SuggestionRule> GetSuggestionRule(Guid suggestionRuleId)
+        public void DeleteSuggestionRule(SuggestionRule rule)
         {
-            return await _dbSet.Include(sr => sr.Foods).FirstOrDefaultAsync(sr => sr.Equals(suggestionRuleId));
+            _context.SuggestionRule.Remove(rule);
+        }
+
+        public async Task<SuggestionRule> GetSuggestionRuleById(Guid id)
+        {
+            return await _context.SuggestionRule
+                .Include(r => r.Foods)
+                .FirstOrDefaultAsync(r => r.Id == id);
         }
 
         public async Task<List<SuggestionRule>> GetSuggestionRules()
         {
-            return await _dbSet.Include(sr => sr.Foods).ToListAsync();
-        }
-
-        public Task<List<SuggestionRule>> GetSuggestionRulesByWeek(int week)
-        {
-            throw new NotImplementedException();
+            return await _context.SuggestionRule
+                .Include(r => r.Foods)
+                .ToListAsync();
         }
     }
+
 }
