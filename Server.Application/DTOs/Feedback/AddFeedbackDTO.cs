47ceb95867f228c5d90c78d18ed407522b9ee285
@dohuuduc315 <dohuuduc315@gmail.com>
2025-08-01 19:55:10 +0700

    add range to rating

--- Server.Application/DTOs/Feedback/AddFeedbackDTO.cs
+++ Server.Application/DTOs/Feedback/AddFeedbackDTO.cs
@@ -1,9 +1,12 @@
-﻿namespace Server.Application.DTOs.Feedback
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Server.Application.DTOs.Feedback
 {
     public class AddFeedbackDTO
     {
         public Guid ClinicId { get; set; }
         public Guid UserId { get; set; }
+        [Range(0, 10, ErrorMessage = "Rating must be between 0 and 10.")]
         public float? Rating { get; set; }
         public string? Comment { get; set; }
     }
--- Server.Application/DTOs/Feedback/UpdateFeedbackDTO.cs
+++ Server.Application/DTOs/Feedback/UpdateFeedbackDTO.cs
@@ -1,8 +1,11 @@
-﻿namespace Server.Application.DTOs.Feedback
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Server.Application.DTOs.Feedback
 {
     public class UpdateFeedbackDTO
     {
         public Guid Id { get; set; }
+        [Range(0, 10, ErrorMessage = "Rating must be between 0 and 10.")]
         public float? Rating { get; set; }
         public string? Comment { get; set; }
     }
