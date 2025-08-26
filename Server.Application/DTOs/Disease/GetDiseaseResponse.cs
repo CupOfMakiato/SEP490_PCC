0fc9b8b323d26b3bff1132b1167491092a1bf33d
Khiem <aidolamgidi@gmail.com>
2025-08-26 09:14:06 +0700

    Crud Disease

---
+++ Server.Application/DTOs/Disease/GetDiseaseResponse.cs
@@ -0,0 +1,15 @@
+﻿
+namespace Server.Application.DTOs.Disease
+{
+    public class GetDiseaseResponse
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Symptoms { get; set; }
+        public string TreatmentOptions { get; set; }
+        public bool PregnancyRelated { get; set; }
+        public string RiskLevel { get; set; }
+        public string TypeOfDesease { get; set; }
+    }
+}
