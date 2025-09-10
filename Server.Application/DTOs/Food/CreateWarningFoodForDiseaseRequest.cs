dac90d9955deea93b2cd95643e6fd7b9b5876f35
Khiem <aidolamgidi@gmail.com>
2025-09-10 11:19:35 +0700

    CreateWarningFoodForDisease, RemoveFoodDisease, RemoveFoodAllergy, CreateWarningFoodForAllergy, CreateRecommendFoodForDisease

---
+++ Server.Application/DTOs/Food/CreateWarningFoodForDiseaseRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace Server.Application.DTOs.Food
+{
+    public class CreateWarningFoodForDiseaseRequest
+    {
+        public Guid DiseaseId { get; set; }
+        public List<FoodDiseaseWarningDto> warningFoodDtos { get; set; }
+    }
+}
